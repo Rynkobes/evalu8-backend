0df091f46d1435121ec51382aeb7cda9d5d7b6c0
Terryb <terrydagbartels@gmail.com>
2022-10-06 17:32:30 +0000

    mongoose package installed and required in index file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const { application } = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
