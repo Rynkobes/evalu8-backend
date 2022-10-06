ecbea920f5976aa9565aade4a356e57f1eb7dadc
Terryb <terrydagbartels@gmail.com>
2022-10-06 20:25:58 +0000

    Connection to evalu8 mongodb added

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const PORT = process.env.PORT || 3001
 const path = __dirname
 
+mongoose.connect("mongodb://localhost:27017/evalu8")
+    .then(() => {
+     console.log('evalu8 db connection open')
+    })
+
+    .catch((err) => {
+        console.log(err)
+    })
+
 app.get('/', (req, res) => res.send('Hello evalu8 backend redone!'))
 app.get('/login', (req, res) => res.send('Welcome to the Login Page!'))
 app.get('*', (req, res) => res.send('Invalid Route!!!'))
