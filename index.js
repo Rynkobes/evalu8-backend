3b106839d98fea2610df0683ae770ebbed4c4947
Terryb <terrydagbartels@gmail.com>
2022-10-06 16:17:08 +0000

    server changed to app following node backend naming convention

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { application } = require('express')
 
-const server = express()
-server.use(bodyParser.urlencoded({ extended: true }))
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3001
 const path = __dirname
 
-server.get('/', (req, res) => res.send('Hello evalu8 backend redone!'))
-server.get('/login', (req, res) => res.send('Welcome to the Login Page!'))
-server.get('*', (req, res) => res.send('Invalid Route!!!'))
+app.get('/', (req, res) => res.send('Hello evalu8 backend redone!'))
+app.get('/login', (req, res) => res.send('Welcome to the Login Page!'))
+app.get('*', (req, res) => res.send('Invalid Route!!!'))
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Evalu8 server listeneing on ${PORT}`)
 })
