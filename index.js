39f21f2e6adac3f30218bd828d37a88201adce05
Terryb <terrydagbartels@gmail.com>
2022-10-08 08:21:16 +0000

    mongoose setup, user and assessment schema added

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
- 
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -10,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const PORT = process.env.PORT || 3001
 const path = __dirname
 
-mongoose.connect("mongodb://localhost:27017/evalu8")
+mongoose.connect("mongodb://localhost:27017/evalu8" )
     .then(() => {
      console.log('evalu8 db connection open')
     })
@@ -18,6 +17,47 @@ mongoose.connect("mongodb://localhost:27017/evalu8")
     .catch((err) => {
         console.log(err)
     })
+
+const userSchema = new mongoose.Schema({
+        email: String,
+        password: String,
+        name: String,
+        type: String,
+        class: String,
+        courses: [String]
+    })
+    
+const User = mongoose.model('User', userSchema);
+const user = new User({
+    name: 'Serena Lucille Bartels',
+    email: 'serena@bemasolutions.com',
+    type: 'Student',
+})
+
+// user.save()
+
+const assessmentSchema = new mongoose.Schema({
+    Title: String,
+    Category: String,
+    Course: String,
+    Teacher: String,
+    Questions: [{
+        question: String,
+        options: {
+
+        }
+    }],
+    correctAnswer: String
+
+})
+
+const Assessment = mongoose.model('Assessment', assessmentSchema)
+const assessment = new Assessment()
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    Teachers: [String]
+})
 
 app.get('/', (req, res) => res.send('Hello evalu8 backend redone!'))
 app.get('/login', (req, res) => res.send('Welcome to the Login Page!'))
